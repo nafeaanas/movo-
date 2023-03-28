c4c53bc9a894e084f6844282f80ef3caf7d38476
nafeaanas <nafeaanass15@gmail.com>
2023-03-28 09:37:10 +0000

    create root react

--- src/Screens/HomeScreen.jsx
+++ src/Screens/HomeScreen.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Layout from '../Layout/Layout'
 
 function HomeScreen() {
   return (
-    <div>HomeScreen</div>
+    <Layout>
+         <div>HomeScreen</div>
+    </Layout>
   )
 }
 
